18370fe1f5d12d16181d6523e66ee52bf0f5fe21
Jorge Marcos <jormarchav@gmail.com>
2021-07-07 13:05:58 +0200

    Add bootstrap compilation stuff

---
+++ docs/compile_bootstrap.sh
@@ -0,0 +1,7 @@
+rm -rf bootstrap/
+git clone https://github.com/twbs/bootstrap
+rm bootstrap/scss/_variables.scss
+cp _variables.scss bootstrap/scss/
+sass bootstrap/scss/bootstrap.scss bootstrap.css
+curl -X POST -s --data-urlencode 'input@bootstrap.css' https://cssminifier.com/raw > css/bootstrap.min.css
+rm bootstrap.css
