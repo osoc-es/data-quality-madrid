5c8656498761ad66106f2c8c6d0eee809fd4c99b
Jorge Marcos <jormarchav@gmail.com>
2021-07-08 13:05:55 +0200

    Add validate form, a few result components

---
+++ docs/js/validator.js
@@ -0,0 +1,11 @@
+function validate() {
+	console.log("Validate button was clicked");
+}
+
+function loadEndpoint() {
+	console.log("Load button was clicked");
+}
+
+function reset() {
+	console.log("Reset button was clicked");
+}
