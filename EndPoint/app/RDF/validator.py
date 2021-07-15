fcb2fe627e7fd1b0eded129c0b91615a6ff88ed3
fbellidopazos <fbellidopazos@gmail.com>
2021-07-15 10:55:47 +0200

    FInal Update

--- EndPoint/app/RDF/validator.py
+++ EndPoint/app/RDF/validator.py
@@ -1,9 +1,10 @@
 import csv
 from datetime import datetime
 import re
+import time
 
 def initializeProcess(csvFile:str):
-
+    start = time.time()
     # Initialize the process
     f = open(csvFile, "r",encoding="ISO8859-1")
     lines = f.readlines()
@@ -34,6 +35,9 @@ def initializeProcess(csvFile:str):
             {
                 "codigopais":[]
             },
+        "CoordGeoGraficas":{
+            "noSeparadas":[]
+        },
         "errorProcessing":"" 
         }
     
@@ -55,6 +59,7 @@ def initializeProcess(csvFile:str):
                 dictErrs["CampoNumerico"]["Region"] += [((rowCount,col), val)] if checkCorrectNumber(val) else []            
                 dictErrs["Fechas"]["formatoFecha"] += [((rowCount,col), val)] if checkFechas(val) else []
                 dictErrs["CampoTelefono"]["codigopais"] += [((rowCount,col), val)] if checkTelephone(val) else []
+                dictErrs["CoordGeoGraficas"]["noSeparadas"] += [((rowCount,col), val)] if checkCoordinates(val) else []
             except Exception as e:
                 print(e)
                 continue
@@ -63,7 +68,8 @@ def initializeProcess(csvFile:str):
         if(rowCount>50):
             break
         rowCount += 1
-    
+    end = time.time()
+    print(f"initializeProcess >> TIME ELAPSE IN CSV : {end-start}")
     return dictErrs
 
 def hasRepetitiveCols(row,rowCount):
@@ -154,7 +160,7 @@ def checkTelephone(value):
   if type(value) == int:
     value = str(value)
     if len(value) == 9:
-      return f"Possible telephone number without prefix: {value}"
+      return True
 
 def checkCorrectNumber(value):
     #Si tenemos un entero, hemos de revisar que en caso de ser negativo no vaya entre parentesis.
@@ -185,3 +191,22 @@ def checkCorrectNumber(value):
                     except Exception:
                         pass
                 return True
+
+def checkCoordinates(value):
+    '''
+    Given a value checks if it is a coordinate
+    '''
+    regex_string =[ 
+        r"(?:(-?[1-8]?d(?:.d{1,18})?|90(?:.0{1,18})?),(-?(?:1[0-7]|[1-9])?d(?:.d{1,18})?|180(?:.0{1,18})?))(?:|(?:(-?[1-8]?d(?:.d{1,18})?|90(?:.0{1,18})?),(-?(?:1[0-7]|[1-9])?d(?:.d{1,18})?|180(?:.0{1,18})?)))*$",
+        r"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$",
+        r"^([-+]?)([\d]{1,2})(((\.)(\d+)(,)))(\s*)(([-+]?)([\d]{1,3})((\.)(\d+))?)$",
+        r"^([-+]?)([\d]{1,3})(((\.)(\d+)(,)))(\s*)(([-+]?)([\d]{1,2})(((\.)(\d+)(,)))(\s*)([-+]?)([\d]{1,3})((\.)(\d+))?)$",
+        r"^(\()([-+]?)([\d]{1,2})(((\.)(\d+)(,)))(\s*)(([-+]?)([\d]{1,3})((\.)(\d+))?(\)))$",
+        r"(-?(90[ :°d]*00[ :\'\'m]*00(\.0+)?|[0-8][0-9][ :°d]*[0-5][0-9][ :\'\'m]*[0-5][0-9](\.\d+)?)[ :\?\"s]*(N|n|S|s)?)[ ,]*(-?(180[ :°d]*00[ :\'\'m]*00(\.0+)?|(1[0-7][0-9]|0[0-9][0-9])[ :°d]*[0-5][0-9][ :\'\'m]*[0-5][0-9](\.\d+)?)[ :\?\"s]*(E|e|W|w)?)"
+        r"""([0-8]?\d(°|\s)[0-5]?\d('|\s)[0-5]?\d(\.\d{1,6})?"?|90(°|\s)0?0('|\s)0?0"?)\s{0,}[NnSs]\s{1,}([0-1]?[0-7]?\d(°|\s)[0-5]?\d('|\s)[0-5]?\d(\.\d{1,6})?"?|180(°|\s)0?0('|\s)0?0"?)\s{0,}[EeOoWw]"""
+        r"^(?:(-?[1-8]?d(?:.d{1,18})?|90(?:.0{1,18})?),(-?(?:1[0-7]|[1-9])?d(?:.d{1,18})?|180(?:.0{1,18})?))(?:|(?:(-?[1-8]?d(?:.d{1,18})?|90(?:.0{1,18})?),(-?(?:1[0-7]|[1-9])?d(?:.d{1,18})?|180(?:.0{1,18})?)))*$"]
+
+    for i in regex_string:
+        if re.match(i, value):
+            return True
+    return False
